342606d07e37e2828d776ba7aea5f71d9d3062ae
Oleksandr Samsonenko <invizibji@gmail.com>
2023-01-18 22:50:16 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix';
-import { getData } from './js/fetchCountries';
+import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
